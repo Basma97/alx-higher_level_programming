c911d71c9590aa1c8a227ec24d5edd4f747c3d4b
basma97 <basmamansour466@gmail.com>
2024-10-01 14:02:59 -0700

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,9 +2,9 @@
 """square module"""
 
 
-class squre:
+class Square:
     """define a square"""
 
     def __init__(self, size):
         """constructor"""
-        self __size = size
+        self.__size = size
